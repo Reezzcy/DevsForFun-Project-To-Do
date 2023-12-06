840522b6bdf882404133198a8d4e550006d71de7
Nicolas Debrito <124766027+Reezzcy@users.noreply.github.com>
2023-12-06 21:21:59 +0700

    Add files via upload

---
+++ assets/script.js
@@ -0,0 +1,28 @@
+function addTask() {
+    var taskInput = document.getElementById("taskInput");
+    var taskList = document.getElementById("taskList");
+
+    if (taskInput.value.trim() !== "") {
+        var li = document.createElement("li");
+        li.innerHTML = taskInput.value.trim() + 
+            '<div class="button-list"> <button class="done-button" onclick="markDoneTask(this)">Mark as Done</button> <button class="delete-button" onclick="removeTask(this)">Delete</button> </div>';
+        taskList.appendChild(li);
+        taskInput.value = "";
+    }
+}
+
+function markDoneTask(button) {
+    var li = button.parentNode.parentNode;
+    li.classList.toggle("done");
+}
+
+function removeTask(button) {
+    var div = button.parentNode.parentNode;
+    var li = div.parentNode;
+    li.parentNode.removeChild(li);
+}
+
+function clearAll() {
+    var taskList = document.getElementById("taskList");
+    taskList.innerHTML = "";
+}
